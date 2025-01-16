2544d47badac37494ddfb928176259c3d47b222d
maeladv <advissemael@gmail.com>
2025-01-16 12:23:29 +0100

    Animation des statistiques

--- insaerospace/main/static/main/scripts/accueil.js
+++ insaerospace/main/static/main/scripts/accueil.js
@@ -23,15 +23,48 @@ function scrollToNext() {
     }
 }
 
+// Fonction pour vérifier si un élément est visible dans la fenêtre d'affichage
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
-
+// Fonction pour démarrer l'animation des nombres
+function startNumberAnimation() {
+    $('.nombre').each(function() {
+        if (isElementInViewport(this) && !$(this).hasClass('animated')) {
+            $(this).rollNumber({
+                number: $(this).data('number'),
+                fontStyle: {
+                    fontSize: 48,
+                    color: '#FFF',
+                    fontFamily: 'Railway',
+                }
+            });
+            $(this).addClass('animated'); // Ajouter une classe pour indiquer que l'animation a déjà eu lieu
+        }
+    });
+}
 
 // ajout de l'event listener au chargeent du caroussel
 function addAutoScroll() {
     setInterval(scrollToNext, 7000);
-
 }
-
 
 // onload 
 window.onload = addAutoScroll();
+
+// Animation des stats
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        startNumberAnimation();
+    });
+
+    // Démarrer l'animation au chargement si les éléments sont déjà visibles
+    startNumberAnimation();
+});
